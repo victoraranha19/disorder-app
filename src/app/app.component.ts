09d19868dbbeed4ff5bf625603c78a3d86e9f86a
victoraranha19 <aldo.victor1610@gmail.com>
2025-04-28 13:02:59 -0300

    🎉configurando angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuLateralComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'disorder-app';
-}
+export class AppComponent {}
---
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-lateral',
+  templateUrl: './menu-lateral.component.html',
+  styleUrl: './menu-lateral.component.css',
+})
+export class MenuLateralComponent {}
