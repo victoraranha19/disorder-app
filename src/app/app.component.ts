93fa973fe48878c9d1d93de3ed4e221458c396eb
victoraranha19 <aldo.victor1610@gmail.com>
2025-04-28 13:36:04 -0300

    🚧salva progresso primeira pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuLateralComponent],
+  imports: [CommonModule, MenuLateralComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  clients = Array.from({ length: 12 }, (_, i) => ({
+    id: i + 1,
+    name: `Cliente ${i + 1}`,
+    favorite: i < 2, // os dois primeiros clientes são favoritos
+  }));
+
+  selectedClient: any = null;
+
+  selectClient(client: any) {
+    this.selectedClient = client;
+  }
+
+  removeClient() {
+    if (this.selectedClient) {
+      this.clients = this.clients.filter(
+        (c) => c.id !== this.selectedClient.id
+      );
+      this.selectedClient = null;
+    }
+  }
+}
--- src/app/components/menu-lateral/menu-lateral.component.ts
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   selector: 'app-menu-lateral',
   templateUrl: './menu-lateral.component.html',
-  styleUrl: './menu-lateral.component.css',
+  styleUrl: './menu-lateral.component.scss',
 })
 export class MenuLateralComponent {}
